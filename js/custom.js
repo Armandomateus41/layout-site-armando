4f972875ab1ebe1ce33faa1c1bd3806db7d4d92e
Armandomateus41 <armandomateus41@gmail.com>
2024-11-28 23:03:44 -0300

    nova as 23-03

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,17 @@ $(function(){
         $('.menu-btn i').toggleClass("active");
     });
 });
+
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+      const answer = button.nextElementSibling;
+      const isOpen = answer.style.display === 'block';
+  
+      // Fechar todos os outros
+      document.querySelectorAll('.faq-answer').forEach(item => item.style.display = 'none');
+  
+      // Alternar o estado atual
+      answer.style.display = isOpen ? 'none' : 'block';
+    });
+  });
+  
